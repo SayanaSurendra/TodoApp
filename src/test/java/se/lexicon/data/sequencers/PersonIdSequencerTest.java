d1c62cf96d5caf4b3b293603436b45d7b4f56028
Sayana Surendran <sayanasurendran31@gmail.com>
2024-11-18 05:54:14 +0100

    Removed unused imports

--- src/test/java/se/lexicon/data/sequencers/PersonIdSequencerTest.java
+++ src/test/java/se/lexicon/data/sequencers/PersonIdSequencerTest.java
@@ -2,7 +2,7 @@ package se.lexicon.data.sequencers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import se.lexicon.model.Person;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
